c7652031b0bc6f0987a497c3335c69a38fcda767
Nathan Johnson <nathanjohnson@Nathans-MBP.lan>
2019-11-24 13:28:01 -0600

    signupValidation

--- SocialApp/SocialApp-Functions/functions/index.js
+++ SocialApp/SocialApp-Functions/functions/index.js
@@ -63,6 +63,22 @@ app.post('/post', (req, res) => {
     });
 });
 
+// check for valid email 
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+if(email.match(regEx)) return true;
+else return false;
+}
+
+// check for empty string function
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
+
+
 // Signup (register) newuser route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -71,10 +87,30 @@ app.post('/signup', (req, res) => {
        confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     };
+
+//declare all errors
+    let errors = {}
+
+// check for email
+if(isEmpty(newUser.email)){
+    errors.email = "email is required"
+} else if(!isEmail(newUser.email)) {
+    errors.email = "Valid email address is required "
+}
+
+// check for password and matching passwordconfirmation
+if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+if(newUser.password !== newUser.confirmPassword) errors.confirmpassword = 'passwords dont match'
+// check user handle
+if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
 let token, userId;
     // VALIDATE DATA 
-    db.doc(`/users/${newUser.handle}`).get()
-        .then(doc => {
+    db.doc(`/users/${newUser.handle}`)
+        .get()
+        .then((doc) => {
             if(doc.exists){
                 return res.status(400).json({ handle: 'this handle is taken' });
             } else {
